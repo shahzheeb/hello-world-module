2e6e1b3acdc09aeb3b17dd538eebe38278602ccb
shahzheeb <shahzheeb@gmail.com>
2018-11-24 10:13:19 -0500

    working

---
+++ hello.world.client/src/com/world/client/HelloWorldClient.java
@@ -0,0 +1,11 @@
+package com.world.client;
+
+import com.hello.HelloWorld;
+
+public class HelloWorldClient {
+
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        System.out.println(hello.sayHello());
+    }
+}
---
+++ hello.world.client/src/module-info.java
@@ -0,0 +1,3 @@
+module hello.world.client {
+    requires hello.world;
+}
